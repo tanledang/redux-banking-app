9b4d6d58bc7556f44fa07e5772252bbea2956837
Your Name <tan.ledang@digitalcrafts.com>
2023-02-07 19:51:57 -0500

    Working redux app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+import { createStore } from 'redux';
+
+console.log("Starting Banking App");
+
+const defaultState = {
+  balance: 0,
+  authorizedUsers: [],
+};
+
+const actionIncrement = {
+	type: 'increment'
+};
+
+const actionDecrement = {
+	type: 'decrement'
+};
+
+const actionSet = {
+  type: 'set',
+  payload: 100
+}
+
+const actionAddAuthorizedUser = {
+  type: 'add-user',
+  payload: {
+    name: "Tan",
+    ssn: "123-456-7890"
+  }
+}
+
+
+function account(state=defaultState, action) {
+  switch(action.type) {
+    case 'increment':
+      return {
+        ...state,
+        balance: state.balance + 1
+      }
+    case 'decrement': 
+      return {
+        ...state,
+        balance: state.balance - 1
+      }
+    case 'set':
+      return {
+        ...state,
+        balance: action.payload
+      }
+    case 'add-user':
+      return {
+        ...state,
+        authorizedUsers: [...state.authorizedUsers, action.payload]
+      }
+    default:
+      return state
+  }
+}
+
+const store = createStore(account);
+
+store.subscribe(() => {
+  console.log('=== state has updated ===');
+  const state = store.getState();
+  console.log(state);
+})
+
+window.store = store;
+window.actionIncrement = actionIncrement;
+window.actionDecrement = actionDecrement;
+window.actionSet = actionSet;
+window.actionAddAuthorizedUser = actionAddAuthorizedUser;
